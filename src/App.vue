4d373f623076ccd99491ba9377fd400bd9d8b76d
Travis Boss <travisboss@gmail.com>
2020-05-20 18:17:41 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <mark-display
+    ref="main"
+    :markdown="markdown"
+    @title="empyreanProject"
+    keyboard-ctrl
+    url-hash-ctrl
+    auto-font-size
+    auto-blank-target
+  ></mark-display>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MarkDisplay from "vue-mark-display";
+import Hammer from "hammerjs";
+
+const markdown = `### Hello, My name is Travis
+*use arrow keys to navigate(desktop)
+
+*swipe left to nativate(mobile)
+
+----
+This was my first attempt at making a slideshow website.
+
+----
+
+Oddly this was one of the hardest things I ever had to build.
+
+----
+
+I really appreciate the oppertunity to interview with Empyrean.
+
+----
+
+If anything it was a great experience talk with the both of you, Wendy and Carlos.
+
+----
+
+<!-- color: red; -->
+<!-- style: font-weight: bold; -->
+
+Carlos, it is great to see another *neovim* user out in the wild :)`;
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  components: { MarkDisplay },
+  data() {
+    return { markdown };
+  },
+  methods: {
+    setTitle({ title }) {
+      document.title = title;
+    }
+  },
+  mounted() {
+    const mc = new Hammer(this.$el);
+    const main = this.$refs.main;
+    mc.on("swipe", event => {
+      if (event.pointerType === "mouse") {
+        return;
+      }
+      switch (event.direction) {
+        case Hammer.DIRECTION_LEFT:
+          main.goNext();
+          return;
+        case Hammer.DIRECTION_RIGHT:
+          main.goPrev();
+          return;
+      }
+    });
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+body {
+  margin: 0;
+  overflow: hidden;
 }
 </style>
